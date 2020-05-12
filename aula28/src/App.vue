94e984a12ceebdeda7eee4a68199a3cdb4bc029e
Miqueias Sousa <miqueias.sousa@padtec.com.br>
2020-05-12 14:39:36 -0300

    Criando o layout da aplicação com bootstrap

--- aula28/src/App.vue
+++ aula28/src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Vue.js</a>
+            <button class="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+      </nav>
     <router-view/>
   </div>
 </template>
@@ -18,6 +29,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-bottom: 0px;
 }
 </style>
---
+++ aula28/src/components/GitHubIssues.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <div class="container">
+        <h1>Vue.js + Github</h1>
+        <p class="lead">
+            Página que lista issues de um repositório do Github, usando Vue.js.
+        </p>
+
+        <div class="alert alert-danger"></div>
+
+        <div class="row">
+            <div class="col">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="github username">
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="github repositório">
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="form-group">
+                    <button class="btn btn-success">GO</button>
+                    <button class="btn btn-danger">LIMPAR</button>
+                </div>
+            </div>
+        </div>
+
+        <br><hr><br>
+
+        <table class="table table-sm table-bordered">
+            <thead>
+            <tr>
+                <th width="100">Número</th>
+                <th>Título</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            <tr>
+                <td class="text-center" colspan="2"><img src="/static/loading.svg" alt=""></td>
+            </tr>
+
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td class="text-center" colspan="2">Nenhuma issue encontrada!</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'GitHubIssues',
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
